75f9f069301d47b0c5a40510d2c8dfccc6f031b4
Royal-Aver <royal_aver@rambler.ru>
2024-02-03 22:31:11 +0300

    DRY for about.html, index.html = base.html

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from main.views import index
+from main.views import index, about
 
 app_name = 'main'
 
-urlpatterns = [path('', index, name='index')]
+urlpatterns = [
+  path('', index, name='index'),
+  path('about/', about, name='about'),
+  ]
