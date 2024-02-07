7a0b5660294951391a5a86bba10e618b9087845f
Royal-Aver <royal_aver@rambler.ru>
2024-02-07 04:21:49 +0300

    add media

--- online_store_app/urls.py
+++ online_store_app/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from online_store_app.settings import DEBUG
+# from online_store_app import settings
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,8 +27,8 @@ urlpatterns = [
     path('goods/', include('goods.urls', namespace='goods')),
 ]
 
-
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
-]
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
