b235be62addac2f703561144cb880903b4fd1efb
Royal-Aver <royal_aver@rambler.ru>
2024-02-10 20:12:44 +0300

    add goods in table

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models
 
 
@@ -15,18 +16,18 @@ class Category(models.Model):
 
 
 class Products(models.Model):
-    title = models.CharField(max_length=255, verbose_name="Наименование")
+    title = models.CharField(max_length=255, unique=True, verbose_name="Наименование")
     slug = models.SlugField(max_length=255, unique=True, verbose_name="URL")
     price = models.DecimalField(
-        max_digits=8, decimal_places=2, default=0.0, verbose_name="Цена"
+        max_digits=6, decimal_places=2, default=0.0, verbose_name="Цена"
     )
     discount = models.DecimalField(
-        max_digits=8, decimal_places=2, verbose_name="Скидка"
+        max_digits=6, decimal_places=2, blank=True, null=True, verbose_name="Скидка в %"
     )
     description = models.TextField(verbose_name="Описание")
     quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
     image = models.ImageField(
-        upload_to="products_images", blank=True, verbose_name="Изображение"
+        upload_to="products_images", blank=True, null=True, verbose_name="Изображение"
     )
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, verbose_name="Категория"
@@ -39,3 +40,14 @@ class Products(models.Model):
 
     def __str__(self):
         return self.title
+
+    def display_id(self):
+        '''Отображение id товара'''
+        return f"{self.id:05}"
+
+    def sale_price(self):
+        '''Цена товара с учетом скидки'''
+        if self.discount:
+            return round(self.price - (self.price * self.discount) / 100 , 2)
+        else:
+            return self.price
