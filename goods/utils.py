16b1aba18dbd3deaf7e4ef1d3776d0f8968a0175
Royal-Aver <royal_aver@rambler.ru>
2024-02-17 19:19:42 +0300

    full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,19 +1,18 @@
-from django.db.models import Q
 from goods.models import Products
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
+
+
 def q_search(query):
-  '''Поиск по id'''
+  '''Поиск по id и полнотекстовый поиск по названию и описанию'''
   if query.isdigit() and len(query) <= 5:
     return Products.objects.filter(id=int(query))
 
+  vector = SearchVector("title", "description")
+  query = SearchQuery(query)
+  result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
 
-  keywords = [word for word in query.split() if len(word) > 2]
+  result = result.annotate(headline=SearchHeadline("title", query, start_sel='<span style="background-color: yellow;">', stop_sel="</span>"))
+  result = result.annotate(bodyline=SearchHeadline("description", query, start_sel='<span style="background-color: yellow;">', stop_sel="</span>"))
 
-  q_objects = Q()
-
-  for token in keywords:
-    q_objects |= Q(description__icontains=token)
-    q_objects |= Q(title__icontains=token)
-
-  return Products.objects.filter(q_objects)
-
+  return result
 
