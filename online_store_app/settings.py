181844c94994be5951300f145a298ef0d1cbe9dc
Royal-Aver <royal_aver@rambler.ru>
2024-02-20 13:55:45 +0300

    first

--- online_store_app/settings.py
+++ online_store_app/settings.py
@@ -83,13 +83,9 @@ AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "home",
-        "USER": "home",
-        "PASSWORD": "home",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
